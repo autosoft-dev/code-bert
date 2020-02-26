c87b3b07de3280009f0e918c7b1c840d1fcd02d4
Shubhadeep Roychowdhury <shubhadeeproychowdhury@gmail.com>
2020-02-26 16:38:20 +0100

    feat(core): final raw data generation code

--- code_bert/core/data_reader.py
+++ code_bert/core/data_reader.py
@@ -17,6 +17,26 @@ def _tokenize_code_string(code_string: str):
         return None
 
 
+def process_string_tokes(tok):
+    s = []
+    lines = tok.split("\n")
+    n = len(lines) if len(lines) <= 4 else 4  # we do not consider more than 4 lines of docstring
+    for line in lines[:n]:
+        toks = line.lstrip().rstrip().split(" ")
+        if toks[0].startswith('"""'):
+            toks = toks[1:]
+            t = ['"""'] + toks
+            toks = t
+        for t in toks:
+            s.append(t.rstrip().lstrip().lower())
+        s.append(spl_tokens[NEWLINE].lower())
+    if s[-1] != spl_tokens[NEWLINE]:
+        s.pop()
+    if s[-1] != '"""':
+        s.append('"""')
+    return s
+
+
 def process_code(code_string):
     g = _tokenize_code_string(code_string)
 
@@ -29,15 +49,8 @@ def process_code(code_string):
                 tok = spl_tokens.get(toknum) if spl_tokens.get(toknum) else tokval
                 if tok.startswith('"""') and prev_tok == "__INDENT__" and toknum == STRING:
                     # It is most likely a docstring.
-                    lines = tok.split("\n")
-                    n = len(lines) if len(lines) <= 4 else 4  # we do not consider more than 4 lines of docstring
-                    for line in lines[:n]:
-                        toks = line.lstrip().rstrip().split(" ")
-                        for t in toks:
-                            s.append(t.rstrip().lstrip().lower())
-                        s.append(spl_tokens[NEWLINE].lower())
-                    if s[-1] != '"""':
-                        s.append('"""')
+                    docstr = process_string_tokes(tok)
+                    s.extend(docstr)
                     prev_tok = tok
                     continue
                 else:
@@ -49,7 +62,10 @@ def process_code(code_string):
                             if not t.startswith("#"):  # If we have in-line comments after the code (like this one)
                                 s.append(t.rstrip().lstrip().lower())
                 else:
-                    s.append(tok.rstrip().lstrip().lower())
+                    if toknum == STRING:
+                        s.extend(process_string_tokes(tok))
+                    else:
+                        s.append(tok.rstrip().lstrip().lower())
         
         return " ".join(s)
     return None
