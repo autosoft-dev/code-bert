7bbc5dee490adc7db3a73a334c9cf060c032dbf9
Shubhadeep Roychowdhury <shubhadeeproychowdhury@gmail.com>
2020-03-03 16:31:52 +0100

    core(data_proc): make the logical line per line type data preparation script

--- code_bert/core/data_reader.py
+++ code_bert/core/data_reader.py
@@ -19,10 +19,13 @@ def _tokenize_code_string(code_string: str):
         return None
 
 
-def process_string_tokes(tok):
+def process_string_tokes(tok, is_docstr=False):
     s = []
     lines = tok.split("\n")
-    n = len(lines) if len(lines) <= 4 else 4  # we do not consider more than 4 lines of docstring
+    if is_docstr:
+        n = len(lines) if len(lines) <= 2 else 2  # we do not consider more than 4 lines of docstring
+    else:
+        n = len(lines)
     for line in lines[:n]:
         toks = line.lstrip().rstrip().split(" ")
         if toks[0].startswith('"""'):
@@ -31,12 +34,44 @@ def process_string_tokes(tok):
             toks = t
         for t in toks:
             s.append(t.rstrip().lstrip().lower())
-        s.append(spl_tokens[NEWLINE].lower())
-    if s[-1] != spl_tokens[NEWLINE]:
-        s.pop()
+        ####################
+        # Not treating the new lines inside STRING type tokens
+        # @TODO - Verify this approach
+        ####################
+        # s.append(spl_tokens[NEWLINE].lower())
+    # if s[-1] != spl_tokens[NEWLINE]:
+    #     s.pop()
     if s[-1] != '"""':
         s.append('"""')
     return s
+
+
+def divide_code_in_logical_lines(s):
+    logical_lines = []
+    substring_arr = []
+    for tok in s:
+        if tok == spl_tokens[NEWLINE].lower():
+            if len(substring_arr) > 256:
+                substring_arr = substring_arr[:255]
+
+            logical_lines.append(" ".join(substring_arr))
+            substring_arr = []
+        else:
+            substring_arr.append(tok)
+    
+    if substring_arr:
+        all_dedents = True
+        for t in substring_arr:
+            if t != spl_tokens[DEDENT].lower():
+                all_dedents = False
+
+        last_part = " ".join(substring_arr)
+        if all_dedents:
+            logical_lines[-1] = logical_lines[-1] + " " + last_part
+        else:
+            logical_lines.append(last_part)
+    
+    return logical_lines
 
 
 def process_code(code_string):
@@ -53,7 +88,7 @@ def process_code(code_string):
                     tok = spl_tokens.get(toknum) if spl_tokens.get(toknum) else tokval
                     if tok.startswith('"""') and prev_tok == "__INDENT__" and toknum == STRING and def_tok_seen:
                         # It is most likely a docstring.
-                        docstr = process_string_tokes(tok)
+                        docstr = process_string_tokes(tok, is_docstr=True)
                         s.extend(docstr)
                         prev_tok = tok
                         def_tok_seen = False
@@ -61,6 +96,9 @@ def process_code(code_string):
                     else:
                         prev_tok = tok
                     if tok != "__INDENT__" and  tok != "__DEDENT__" and tok != "__NEWLINE__":
+                        if toknum == STRING:
+                            s.extend(process_string_tokes(tok))
+                            continue
                         if tok == "def":
                             def_tok_seen = True
                         toks = split_identifier_into_parts(tok)
@@ -69,12 +107,10 @@ def process_code(code_string):
                                 if not t.startswith("#"):  # If we have in-line comments after the code (like this one)
                                     s.append(t.rstrip().lstrip().lower())
                     else:
-                        if toknum == STRING:
-                            s.extend(process_string_tokes(tok))
-                        else:
-                            s.append(tok.rstrip().lstrip().lower())
+                        s.append(tok.rstrip().lstrip().lower())
             
-            return " ".join(s)
+            # return " ".join(s)
+            return divide_code_in_logical_lines(s)
         except Exception:
             return None
     return None
